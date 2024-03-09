bbec7d616193081e5054c8270c1290b01aa7ad9e
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-03-09 22:02:24 +0600

    pattern 8 & 9

--- pattern-8.c
+++ pattern-8.c
@@ -1,25 +1,22 @@
 /*
-    9 7 5 3 1
+    2 4 6 8 10
+    12 14 16 18 20
 */
 #include <stdio.h>
 
 int main(void)
 {
-    int i, j, k = 1, n = 5;
+    int i, j, k = 2, n = 5;
 
     for(i = 1; i <= n; i++)
     {
         for(j = 1; j <= n; j++)
         {
-            if(k % 2 != 0)
-            {
-                printf("%2d ", k);
-                k += 2;
-            }
+            printf("%2d ", k);
+            k += 2;
         }
         printf("\n");
     }
 
     return 0;
 }
-
--- pattern-9.c
+++ pattern-9.c
@@ -1,17 +1,23 @@
 /*
-    1 2 3 4 5
-    6 7 8 9 10
+   1 2 3 4 5
+   2 4 6 8 10
+   3 6 9 12 15
 */
 #include <stdio.h>
 
 int main(void)
 {
-    int i, j, k = 1, n = 5;
+    int i, j, k = 2, n = 5;
 
     for(i = 1; i <= n; i++)
     {
         for(j = 1; j <= n; j++)
-            printf("%2d ", k++);
+        {
+            printf("%2d ", i*j);
+        }
         printf("\n");
     }
+
+    return 0;
 }
+
